d0b97b54715fa2be2cd98d63affed4db52096435
Douglas <133309975+douglas2569@users.noreply.github.com>
2024-06-17 20:31:20 -0300

    update SignUp

--- app/src/main/java/com/example/uniclass/views/SignUp.kt
+++ app/src/main/java/com/example/uniclass/views/SignUp.kt
@@ -7,9 +7,9 @@ import com.example.uniclass.compoments.TopBar
 import com.example.uniclass.compoments.UnitComponentTopBar
 
 @Composable
-fun SignUp(modifer: Modifier = Modifier) {
-  Column {
-    TopBar("Log in", { UnitComponentTopBar("Left", {}) }, { UnitComponentTopBar("Right", {}) })
+fun SignUp(modifer: Modifier = Modifier, onSignUpClick:()->Unit) {
+  Column(modifier = modifer) {
+    TopBar("Sign Up", { UnitComponentTopBar("Left", {}) }, { UnitComponentTopBar("Right", {}) })
   }
 
 }
